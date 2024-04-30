d63013f370e4210ddae1785b27cffa7d076d1dd0
Kyra Moore <kyra.moore21@yahoo.com>
2024-04-30 16:13:19 -0400

    added validation

--- HighOrLow/Program.cs
+++ HighOrLow/Program.cs
@@ -4,13 +4,26 @@
 Random rand = new Random();
 int num = rand.Next(1, 101);
 
+//guess counter
 int guess = 0;
 
 while (true)
 {
+    //user first guess
     Console.WriteLine("Guess a number between 1 and 100");
-    int userInt = int.Parse(Console.ReadLine()); 
+    int userInt = int.Parse(Console.ReadLine());
+
+    //validation
+    if (userInt < 1 || userInt > 100)
+    {
+        Console.WriteLine("Invalid Input");
+        continue;
+    } 
+
+    //counting everytime it loops with a valid guess
     guess++;
+
+    //checking if guess is correct
     if (userInt > num) 
     {
         Console.WriteLine("Guessed too high.");
