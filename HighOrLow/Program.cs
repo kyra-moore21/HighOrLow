dfcf5d79f094e130bcc7e3ebdab615a3e33f5fb4
Kyra Moore <kyra.moore21@yahoo.com>
2024-04-30 15:59:53 -0400

    first commit

---
+++ HighOrLow/Program.cs
@@ -0,0 +1,32 @@
+﻿ 
+
+//random number 
+Random rand = new Random();
+int num = rand.Next(1, 101);
+
+int guess = 0;
+
+while (true)
+{
+    Console.WriteLine("Guess a number");
+    int userInt = int.Parse(Console.ReadLine()); 
+    guess++;
+    if (userInt > num) 
+    {
+        Console.WriteLine("Guessed too high.");
+    } 
+    else if (userInt < num)
+    {
+        Console.WriteLine("Guessed too low");
+    }
+    else
+    {
+        Console.WriteLine($"You guessed it right! it took you {guess} tries!");
+        break;
+    }
+
+
+}
+
+
+
